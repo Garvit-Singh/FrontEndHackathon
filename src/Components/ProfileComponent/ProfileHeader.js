e398231cd6f254b6ca14f608eefe78afd9f27ee5
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 22:59:32 +0530

    Update ProfileHeader.js

--- src/Components/ProfileComponent/ProfileHeader.js
+++ src/Components/ProfileComponent/ProfileHeader.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function ProfileHeader() {
-  return (
-    <React.Fragment>
-      <div className="text-left profile-header" id="ngo-id">
-        <h3 className="heading-details ngo-name">
-          ngo-name
-        </h3>
-        <p className="para-details ngo-aim">
-          ngo-aim
-        </p>
-        <p className="para-details ngo-description">
-          ngo-description
-        </p>
-        <p className="para-details ngo-catergory">
-          ngo-category
-        </p>
-        <p className="para-details ngo-contact_Info">
-          ngo-contact_Info
-        </p>
-      </div>
-    </React.Fragment>
-  )
+class ProfileHeader extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <React.Fragment>
+        <div className="text-left profile-header" id="ngo-id">
+          <h3 className="heading-details ngo-name">
+            {this.props.ngoName}
+          </h3>
+          <p className="para-details ngo-aim">
+            {this.props.ngoAim}
+          </p>
+          <p className="para-details ngo-description">
+            {this.props.ngoDesc}
+          </p>
+          <p className="para-details ngo-catergory">
+            {this.props.ngoCat}
+          </p>
+          <p className="para-details ngo-contact_Info">
+            Contact Info:
+          </p>
+          <p className="para-details ngo-contact_Info">
+            {this.props.ngoName}@gmail.com
+          </p>
+          <p className="para-details ngo-contact_Info">
+            9238792398
+          </p>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default ProfileHeader
