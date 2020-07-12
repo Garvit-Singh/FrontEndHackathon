9d97c31e4548b0ebe331024ee2cd3b9f91eebb8e
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 11:41:40 +0530

    Update Profile.js

--- src/Components/ProfileComponent/Profile.js
+++ src/Components/ProfileComponent/Profile.js
@@ -2,13 +2,17 @@ import React from 'react'
 import './profile.css'
 import ProfileHeader from './ProfileHeader'
 import ProfileActivity from './ProfileActivity'
+import ProfileSideTransactionPanel from './ProfileSideTransactionPanel';
 
 function Profile() {
   return (
     <React.Fragment>
       Profile
       <ProfileHeader />
-      <ProfileActivity />
+      <div className="row no-gutters d-flex justify-content-between">
+				<ProfileActivity />
+				<ProfileSideTransactionPanel />
+			</div>
     </React.Fragment>
   )
 }
