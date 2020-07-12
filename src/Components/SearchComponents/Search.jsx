cb69f43a6d97939d860235b90f540951e9748e15
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 23:02:13 +0530

    Update Search.jsx

--- src/Components/SearchComponents/Search.jsx
+++ src/Components/SearchComponents/Search.jsx
@@ -3,29 +3,13 @@ import SingleCard from './SearchSingleCard';
 import './search.css';
 
 class SearchPage extends Component {
-	state = {
-		ngo: [
-			{
-				name: 'NGO 1'
-			},
-			{
-				name: 'NGO 2'
-			},
-			{
-				name: 'NGO 3'
-			},
-			{
-				name: 'NGO 4'
-			}
-		]
-	};
 	render() {
 		return (
 			<React.Fragment>
 				<div className="container mt-5">
 				<div className="row no-gutters clearfix mt-5">
-					{this.state.ngo.map((ngo, key) => {
-						return <SingleCard ngo={this.state.ngo} />;
+					{this.props.users.map((user, key) => {
+						return <SingleCard user={user} />;
 					})}
 				</div>
 				</div>
