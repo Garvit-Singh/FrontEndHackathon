13ec0b8d059b148378f189cc1d96c2109254b09d
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 22:57:23 +0530

    Update Page.js

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import Home from "./HomeComponent/Home";
 import Profile from "./ProfileComponent/Profile";
 import Search from "./SearchComponents/Search";
@@ -6,13 +7,28 @@ import Login from './LoginComponent/Login';
 import Register from './RegistrationComponent/Register';
 
 class Pages extends Component {
+    state = {
+        users: []
+    };
+
+    componentDidMount() {
+        Axios.get('https://backendngodonor.herokuapp.com/users')
+        .then((res) => {
+            this.setState({ users: res.data.users });
+            console.log("Requests Successful")
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
     render() { 
         if(this.props.flag === 0) {
             return(<Home/>)
         } else if(this.props.flag === 1) {
             return(<Profile/>)
         } else if(this.props.flag === 2) {
-            return(<Search/>)
+            return(<Search users={this.state.users}/>)
         } else if( this.props.flag === 3) {
             return (<Login />)
         } else if( this.props.flag === 4) {
