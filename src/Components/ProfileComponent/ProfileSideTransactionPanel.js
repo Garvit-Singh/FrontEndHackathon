64ec727002c90acb0bdb76e2df5cbb16a215c03e
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 23:00:30 +0530

    Update ProfileSideTransactionPanel.js

--- src/Components/ProfileComponent/ProfileSideTransactionPanel.js
+++ src/Components/ProfileComponent/ProfileSideTransactionPanel.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import image from '../Images/young-tomato-5808.jpg'
 
-function ProfileSideTransactionPanel() {
-  return (
-		<React.Fragment>
-			<div className="d-flex justify-content-center transaction-panel">
-				<div id="NGO_Logo" className="card text-center" style={{ width: '18rem' }}>
-					<img src={image} className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">NGO Name</h5>
-						<a href="#1" className="btn btn-outline-success">
-							Donate
-						</a>
+class ProfileSideTransactionPanel extends Component {
+	render(){
+		console.log(this.props);
+		return (
+				<React.Fragment>
+					<div className="d-flex justify-content-center transaction-panel">
+						<div id="NGO_Logo" className="card text-center" style={{ width: '18rem' }}>
+							<img src={image} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Help Us Grow</h5>
+								<a href="#1" className="btn btn-outline-success">
+									Donate
+								</a>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</React.Fragment>
-  )
+				</React.Fragment>
+		)
+		}
 }
 
 export default ProfileSideTransactionPanel
