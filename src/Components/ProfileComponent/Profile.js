e5fb2edb0ca69fcd3705b7180e5fd9eda28f637d
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 22:59:01 +0530

    Update Profile.js

--- src/Components/ProfileComponent/Profile.js
+++ src/Components/ProfileComponent/Profile.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './profile.css'
 import ProfileHeader from './ProfileHeader'
 import ProfileActivity from './ProfileActivity'
+import Axios from 'axios'
 
-function Profile() {
-  return (
-    <React.Fragment>
-      Profile Page
-      <ProfileHeader />
-      <ProfileActivity />
-    </React.Fragment>
-  )
+class Profile extends Component {
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {
+    Axios.get(`https://backendngodonor.herokuapp.com/users/ngo/${this.props.userId}`)
+    .then((res) => {
+      this.setState({
+        user: res.data.user
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ProfileHeader ngoName={this.state.user.ngoName}
+        ngoAim={this.state.user.ngoAim}
+        ngoDesc={this.state.user.ngoDescription}
+        ngoCat={this.state.user.ngoCategory}
+        contact={this.state.user.contact_Info}/>
+        <ProfileActivity ngoName={this.state.user.ngoName}/>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Profile
