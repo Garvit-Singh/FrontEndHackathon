4b7e3533a94bd4fdc44d2d5e12e7f58e78c7bb50
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 22:57:54 +0530

    Update Login.js

--- src/Components/LoginComponent/Login.js
+++ src/Components/LoginComponent/Login.js
@@ -1,39 +1,33 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form'
 
-function Login() {
+function HomeFotter() {
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="row no-gutters d-flex justify-content-center mt-5">
-          <div class="card text-center mt-5">
-            <div class="card-header">
-              Member Login
-            </div>
-            <div class="card-body"><Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <a href="#" class="btn btn-primary">Login</a>
-            </Form>
-            </div>
-          </div>
+      <div className="row no-gutters bg-dark fotter">
+        <div className="col-md-6">
+          <h5 className="fotter-title">About Us</h5>
+          {/* <p className=""> */}
+<div className="list-group fotter-content">
+  <button type="button" className="list-group-item bg-dark list-color">Work from Home</button>
+  <button type="button" className="list-group-item bg-dark list-color">BITS PILANI, GOA CAMPUS</button>
+  <button type="button" className="list-group-item bg-dark list-color">Hackathon Project</button>
+  <button type="button" className="list-group-item bg-dark list-color">Contact Details</button>
+</div>
+          {/* </p> */}
+        </div>
+        <div className="col-md-6">
+          <h5 className="fotter-title">Contributors</h5>
+          {/* <p className="fotter-content"> */}
+<div className="list-group fotter-content">
+  <button type="button" className="list-group-item bg-dark list-color">Vishal</button>
+  <button type="button" className="list-group-item bg-dark list-color">Abhay</button>
+  <button type="button" className="list-group-item bg-dark list-color">Garvit</button>
+</div>
+          {/* </p> */}
         </div>
       </div>
     </React.Fragment>
   )
 }
 
-export default Login
+export default HomeFotter
