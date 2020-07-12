eb9de7c572be23019405d19de7a07d1207a1057e
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-12 11:41:02 +0530

    Update ProfileSideTransactionPanel.js

--- src/Components/ProfileComponent/ProfileSideTransactionPanel.js
+++ src/Components/ProfileComponent/ProfileSideTransactionPanel.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import image from '../Images/download.jpg';
+import './profile.css';
 
 function ProfileSideTransactionPanel() {
-  return (
-    <React.Fragment>
-      Profile Transcation panel left side
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<div className="col-md-3 mt-4">
+				<div id="NGO_Logo" className="card text-center" style={{ width: '18rem' }}>
+					<img src={image} className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">NGO Name</h5>
+						<a href="#" className="btn btn-primary">
+							Donate
+						</a>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
 
-export default ProfileSideTransactionPanel
+export default ProfileSideTransactionPanel;
