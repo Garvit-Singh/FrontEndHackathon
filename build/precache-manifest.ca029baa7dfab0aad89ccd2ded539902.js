03bc33a2993d6040d24e78d25579cb8f2218c8bb
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-07-20 19:11:48 +0530

    Add files via upload

---
+++ build/precache-manifest.ca029baa7dfab0aad89ccd2ded539902.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4721e046f43e4633b38607fcf79ae0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ff8f494e77498a413e",
+    "url": "/static/css/main.6fc77359.chunk.css"
+  },
+  {
+    "revision": "804766998523b6906bb0",
+    "url": "/static/js/2.e5c68638.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e5c68638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ff8f494e77498a413e",
+    "url": "/static/js/main.d2c8beb5.chunk.js"
+  },
+  {
+    "revision": "1f5a72f312e2a30b4989",
+    "url": "/static/js/runtime-main.7e652ce6.js"
+  },
+  {
+    "revision": "9000528f8e306f14fb8c19a703c42342",
+    "url": "/static/media/agriculture-close-up-cultivation-ecology-421999.9000528f.jpg"
+  },
+  {
+    "revision": "0bdb03637e39474f902cd799a9928220",
+    "url": "/static/media/brown-painted-wall-on-dim-light-1055613.0bdb0363.jpg"
+  },
+  {
+    "revision": "2c1bb96bdd4762e7d398c1266dc7918f",
+    "url": "/static/media/nature-sky-clouds-field-9198.2c1bb96b.jpg"
+  },
+  {
+    "revision": "12ef51e67b949243f2665cd2856b2a94",
+    "url": "/static/media/young-tomato-5808.12ef51e6.jpg"
+  }
+]);
